43b515ee6058dc1f85235f298521220df723ef41
Trilochan Sashank Yeturi <sharmasashi287@gmail.com>
2024-05-25 11:34:45 +0530

    easy problems

---
+++ Python/Easy/Temp_converter.py
@@ -0,0 +1,8 @@
+def celsius_to_fahrenheit(c: float) -> float:
+    return (c * 9/5) + 32
+
+def fahrenheit_to_celsius(f: float) -> float:
+    return (f - 32) * 5/9
+
+print(celsius_to_fahrenheit(25))  # Expected output: 77.0 Fahrenheit
+print(fahrenheit_to_celsius(77))  # Expected output: 25.0 Celsius
---
+++ Python/Easy/length_of_lastword.py
@@ -0,0 +1,4 @@
+def length_of_lastword(sentence: str) -> int:
+    return len(sentence.split()[-1]) if sentence.split() else 0
+
+print(length_of_lastword("I am Sashi"))  # Expected output: 5
